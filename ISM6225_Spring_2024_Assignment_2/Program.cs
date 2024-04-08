22c9c5f3d6d70a04e3eace6f355bc1dac8a7cdf9
Yashwanth Reddy Solipuram <solipuram12@usf.edu>
2024-04-07 22:43:18 -0400

    Latest commit

--- ISM6225_Spring_2024_Assignment_2/Program.cs
+++ ISM6225_Spring_2024_Assignment_2/Program.cs
@@ -103,7 +103,7 @@ namespace ISM6225_Spring_2024_Assignment_2
                     return 0;
                     int i = 0;
                     
-                    // iterate the loop 
+                    // iterate the loopp
                     for (int j = 1; j < nums.Length; j++)
                     {
                         if (nums[j] != nums[i]) //when j is not equal to i
